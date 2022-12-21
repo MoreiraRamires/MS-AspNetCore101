6e1b80a85e79cb96e13f5325338657bdb6c92b2c
Rafael Ramires <moreira.ramires@gmail.com>
2022-12-21 13:34:37 -0300

    Add existing project files to Git

---
+++ ContosoCrafts.WebSite/Controllers/ProductsController.cs
@@ -0,0 +1,37 @@
+﻿using ContosoCrafts.WebSite.Models;
+using ContosoCrafts.WebSite.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ContosoCrafts.WebSite.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        public ProductsController(JsonFileProductService productService)
+        {
+            this.ProductService = productService;
+        }
+
+        public  JsonFileProductService ProductService { get; set; }
+
+        [HttpGet]
+        public IEnumerable<Product> Get()
+        {
+            return
+                ProductService.GetProducts();
+        }
+
+        [Route("Rate")]
+        [HttpGet]
+        public ActionResult Get( [FromQuery]string ProductId, int Rating)
+        {
+            ProductService.AddRating(ProductId, Rating);
+            return Ok();
+
+        }
+
+    }
+}
